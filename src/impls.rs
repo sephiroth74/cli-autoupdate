631dec92dd4e1ae26729d2ee43b29a6b96b8838a
Alessandro Crugnola <alessandro.crugnola@swisscom.com>
2024-01-14 12:52:22 +0100

    initial commit

---
+++ src/impls.rs
@@ -0,0 +1,100 @@
+use std::cmp::min;
+use std::fs::File;
+use std::io::{ErrorKind, Write};
+use std::path::PathBuf;
+
+use flate2::read::GzDecoder;
+use futures_util::stream::StreamExt;
+use indicatif::{MultiProgress, ProgressBar, ProgressStyle};
+use semver::Version;
+use serde::{Deserialize, Deserializer};
+use tar::Archive;
+use tracing::{debug, trace};
+use url::Url;
+
+use crate::{Config, Error, Registry, RemoteVersion};
+
+pub(crate) async fn fetch_remote_version<C: Config, R: Registry>(config: &C, registry: &R) -> crate::Result<RemoteVersion> {
+	let url = registry
+		.get_base_url()
+		.join(registry.get_update_path(config.into()).as_str())?;
+
+	trace!("url: {}", url);
+
+	reqwest::get(url)
+		.await?
+		.json::<RemoteVersion>()
+		.await
+		.map_err(|err| Error::ReqwestError(err))
+}
+
+pub async fn extract(src: &PathBuf, dst: &PathBuf) -> crate::Result<()> {
+	let src_filename = src.file_name().ok_or(std::io::Error::from(ErrorKind::NotFound))?;
+	debug!("Decompressing {:?} → {:?}", src_filename, dst);
+	let tar_gz = File::open(src)?;
+	let tar = GzDecoder::new(tar_gz);
+	let mut archive = Archive::new(tar);
+	Ok(archive.unpack(dst)?)
+}
+
+pub async fn download_file(
+	client: &reqwest::Client,
+	url: &Url,
+	path: &PathBuf,
+	multi_progress: Option<MultiProgress>,
+	progress_style: Option<ProgressStyle>,
+) -> crate::Result<()> {
+	// Reqwest setup
+	let filename = url.path_segments().unwrap().last().unwrap().to_string();
+	let res = client.get(url.to_string().as_str()).send().await?;
+	let total_size = res
+		.content_length()
+		.ok_or(crate::Error::InvalidContentLengthError(url.to_string()))?;
+
+	// Indicatif setup
+
+	let pb = if let Some(multi_progress) = multi_progress {
+		let pb = multi_progress.add(ProgressBar::new(total_size));
+		if let Some(style) = progress_style {
+			pb.set_style(style.clone());
+		} else {
+			pb.set_style(ProgressStyle::default_bar());
+		}
+		pb.set_prefix("Downloading".to_string());
+		pb.set_message(filename);
+		Some(pb)
+	} else {
+		None
+	};
+
+	// download chunks
+	let mut file = File::create(path)?;
+	let mut downloaded: u64 = 0;
+	let mut stream = res.bytes_stream();
+
+	while let Some(item) = stream.next().await {
+		let chunk = item?;
+		file.write_all(&chunk)?;
+		let new = min(downloaded + (chunk.len() as u64), total_size);
+		downloaded = new;
+
+		if let Some(pb) = &pb {
+			pb.set_position(new);
+		}
+	}
+
+	if let Some(pb) = &pb {
+		pb.finish();
+	}
+
+	debug!("Downloaded {} to {:?}", url, path);
+	return Ok(());
+}
+
+pub(crate) fn value_to_version<'de, D>(deserializer: D) -> std::result::Result<Version, D::Error>
+where
+	D: Deserializer<'de>,
+{
+	let s: &str = Deserialize::deserialize(deserializer)?;
+	Version::parse(s).map_err(|err| serde::de::Error::custom(err.to_string()))
+}
---
+++ src/lib.rs
@@ -0,0 +1,205 @@
+use std::env::temp_dir;
+use std::io::ErrorKind;
+use std::path::{Path, PathBuf};
+
+use chrono::{DateTime, Utc};
+use indicatif::style::TemplateError;
+use indicatif::{MultiProgress, ProgressStyle};
+use reqwest::Url;
+use semver::Version;
+use thiserror::Error;
+
+mod impls;
+
+#[derive(Error, Debug)]
+pub enum Error {
+	#[error(transparent)]
+	ReqwestError(#[from] reqwest::Error),
+
+	#[error(transparent)]
+	UrlParseError(#[from] url::ParseError),
+
+	#[error("Invalid version")]
+	InvalidVersionError,
+
+	#[error(transparent)]
+	IoError(#[from] std::io::Error),
+
+	#[error(transparent)]
+	TemplateError(#[from] TemplateError),
+
+	#[error("Failed to get content length from '{0}'")]
+	InvalidContentLengthError(String),
+}
+
+pub trait Registry {
+	fn get_base_url(&self) -> Url;
+
+	fn get_update_path<C: Config>(&self, config: &C) -> String;
+}
+
+pub trait Config {
+	fn version(&self) -> Version;
+	fn target(&self) -> String;
+}
+
+#[derive(Debug, serde::Deserialize)]
+pub struct RemoteVersion {
+	#[serde(deserialize_with = "impls::value_to_version")]
+	pub version: Version,
+	pub checksum: String,
+	pub size: usize,
+	pub path: String,
+	pub datetime: DateTime<Utc>,
+}
+
+pub type Result<T> = std::result::Result<T, crate::Error>;
+
+pub async fn check_version<C: Config, R: Registry>(config: &C, registry: &R) -> Result<(bool, RemoteVersion)> {
+	impls::fetch_remote_version(config, registry)
+		.await
+		.and_then(|r| Ok((r.version > config.version(), r)))
+}
+
+pub async fn update_self<C: Config, R: Registry>(
+	config: &C,
+	registry: &R,
+	multi_progress: Option<MultiProgress>,
+	progress_style: Option<ProgressStyle>,
+) -> Result<()> {
+	let result = check_version(config, registry).await?;
+	let remote_version = result.1;
+	if result.0 {
+		let remote_path = remote_version.path;
+		let remote_file_path = PathBuf::from(&remote_path);
+		let filename = remote_file_path
+			.file_name()
+			.ok_or(Error::IoError(std::io::Error::from(ErrorKind::NotFound)))?;
+		let target_path = temp_dir().join(filename);
+		let remote_path = registry.get_base_url().join(&remote_path.as_str())?;
+		let client = reqwest::ClientBuilder::default().build().unwrap();
+
+		let _ = impls::download_file(&client, &remote_path, &target_path, multi_progress, progress_style).await?;
+
+		let bin_name = std::env::current_exe().or(Err(Error::IoError(std::io::Error::from(ErrorKind::NotFound))))?;
+		let bin_name_path = bin_name.parent().unwrap_or(Path::new("/")).to_path_buf();
+		impls::extract(&target_path, &bin_name_path).await
+	} else {
+		Err(Error::InvalidVersionError)
+	}
+}
+
+#[cfg(test)]
+mod tests {
+	use console::Style;
+	use indicatif::{MultiProgress, ProgressStyle};
+	use reqwest::Url;
+	use semver::Version;
+	use tracing::level_filters::LevelFilter;
+	use tracing::subscriber;
+	use tracing_subscriber::prelude::*;
+	use tracing_subscriber::EnvFilter;
+
+	use crate::{check_version, update_self, Config, Registry};
+
+	struct LabRegistry;
+
+	struct LabConfig;
+
+	impl Registry for LabRegistry {
+		fn get_base_url(&self) -> Url {
+			Url::parse(format!("https://test.example.com/aot/").as_str()).unwrap()
+		}
+
+		fn get_update_path<C: Config>(&self, config: &C) -> String {
+			format!("{}.json", config.target())
+		}
+	}
+
+	impl Config for LabConfig {
+		fn version(&self) -> Version {
+			let version_str = std::env::var("CARGO_PKG_VERSION").unwrap();
+			Version::parse(version_str.as_str()).unwrap()
+		}
+
+		fn target(&self) -> String {
+			let target = std::env::var("TARGET").unwrap_or("aarch64-apple-darwin".to_string());
+			format!("{}", target)
+		}
+	}
+
+	#[tokio::test]
+	async fn test_check_version() {
+		let config = LabConfig;
+		let registry = LabRegistry;
+		let (has_update, version) = check_version(&config, &registry).await.unwrap();
+
+		if has_update {
+			let bin_name = console::style("binary_name").cyan().italic().bold();
+			let this_version = config.version();
+			let other_version = console::style(version.version).green();
+			let update_url = registry.get_base_url().join(version.path.as_str()).unwrap();
+
+			println!(
+				"A new release of {} is available: {} → {}",
+				bin_name, this_version, other_version
+			);
+			println!("Released on {}", version.datetime);
+			println!("{}", console::style(update_url).yellow());
+		}
+	}
+
+	#[tokio::test]
+	async fn test_update_version() {
+		init_logging();
+		let current_exe = std::env::current_exe();
+		println!("current exe: {:?}", current_exe);
+
+		let config = LabConfig;
+		let registry = LabRegistry;
+
+		let progress_style =
+			ProgressStyle::with_template("{prefix:.green.bold} [{bar:40.cyan/blue.bold}] {percent:>5}% [ETA {eta}] {msg} ")
+				.unwrap()
+				.progress_chars("=> ");
+		let multi_progress = MultiProgress::new();
+
+		let result = update_self(&config, &registry, Some(multi_progress), Some(progress_style)).await;
+
+		match result {
+			Ok(_) => {}
+			Err(err) => {
+				println!("Error checking for version update:");
+				println!("{}", console::style(err.to_string()).red().italic());
+			}
+		}
+	}
+
+	fn init_logging() {
+		let registry = tracing_subscriber::Registry::default();
+
+		let term_subscriber = logging_subscriber::LoggingSubscriberBuilder::default()
+			.with_time(true)
+			.with_level(true)
+			.with_target(true)
+			.with_file(false)
+			.with_line_number(false)
+			.with_min_level(LevelFilter::TRACE)
+			.with_format_level(logging_subscriber::LevelOutput::Long)
+			.with_default_style(Style::default().dim())
+			.with_level_style_warn(Style::new().color256(220).bold())
+			.with_level_style_trace(Style::new().magenta().bold())
+			.with_date_time_style(Style::new().white())
+			.build();
+
+		let filter = EnvFilter::builder()
+			.with_default_directive(LevelFilter::TRACE.into())
+			.from_env()
+			.unwrap()
+			.add_directive("hyper::proto=warn".parse().unwrap())
+			.add_directive("hyper::client=warn".parse().unwrap());
+
+		let subscriber = registry.with(filter).with(term_subscriber);
+		subscriber::set_global_default(subscriber).unwrap();
+	}
+}
